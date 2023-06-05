1d0337d32f40b781f9b7509cb40448b81bde6b00
Ackerley Tng <ackerleytng@google.com>
2023-06-05 00:45:52 +0000

    mm: hugetlb: Parametrize hugetlb functions by resv_map
    Parametrize remove_mapping_hugepages() and hugetlb_unreserve_pages()
    by resv_map to remove the use of inode_resv_map() and decouple hugetlb
    with hugetlbfs.
    
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -6954,9 +6954,9 @@ out_err:
  * Returns 0 on success.
  */
 long hugetlb_unreserve_pages(struct hstate *h, struct hugepage_subpool *spool,
-			     struct inode *inode, long start, long end, long freed)
+			     struct resv_map *resv_map, struct inode *inode,
+			     long start, long end, long freed)
 {
-	struct resv_map *resv_map = inode_resv_map(inode);
 	long chg = 0;
 	long gbl_reserve;
 
