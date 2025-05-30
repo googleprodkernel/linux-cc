4574b4e02ec03385c3f34be3f8cacb5329e89daf
Ackerley Tng <ackerleytng@google.com>
2025-05-30 07:57:08 -0700

    mm: mmap: Get alignment mask using new file operation
    Introduce new file operation .get_align_mask to allow filesystems to
    specify alignment masks for mmap.
    
    This converts special-casing based on is_file_hugepages() (or
    FOP_HUGE_PAGES) to be more generic.
    
    huge_page_mask_align() was used to compute alignment mask based on
    FOP_HUGE_PAGES when HugeTLB pages are used, but huge_page_mask_align()
    assumes that hstate is stored at a specific place based on the file
    pointer.
    
    With this change, filesystems can more flexibly determine alignment
    masks for mmap.
    
    Change-Id: I9403a3989d632a0695e289719f7a270cb1d2a177
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -1067,7 +1067,8 @@ void hugetlb_unregister_node(struct node *node);
  */
 bool is_raw_hwpoison_page_in_hugepage(struct page *page);
 
-static inline unsigned long huge_page_mask_align(struct file *file)
+static inline unsigned long huge_page_mask_align(struct file *file,
+						 unsigned long flags)
 {
 	return PAGE_MASK & ~huge_page_mask(hstate_file(file));
 }
@@ -1075,7 +1076,8 @@ static inline unsigned long huge_page_mask_align(struct file *file)
 #else	/* CONFIG_HUGETLB_PAGE */
 struct hstate {};
 
-static inline unsigned long huge_page_mask_align(struct file *file)
+static inline unsigned long huge_page_mask_align(struct file *file,
+						 unsigned long flags)
 {
 	return 0;
 }
