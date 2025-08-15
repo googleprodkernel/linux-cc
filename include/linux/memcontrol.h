9ead73e78d36afcd3865eea289477fa0c0d9ee86
Ackerley Tng <ackerleytng@google.com>
2025-08-15 16:14:14 -0700

    mm: memcontrol: Remove now-unused function mem_cgroup_charge_hugetlb
    With the (re)introduction of the try-commit-cancel charging protocol for
    HugeTLB's use, mem_cgroup_charge_hugetlb() is now redundant.
    
    Remove the function's implementation from mm/memcontrol.c and its
    declaration from include/linux/memcontrol.h
    
    No functional change intended.
    
    Change-Id: Ic4652c7a1742190d65d2ff34c6043bf5fc4fee41
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -659,8 +659,6 @@ static inline int mem_cgroup_charge(struct folio *folio, struct mm_struct *mm,
 	return __mem_cgroup_charge(folio, mm, gfp);
 }
 
-int mem_cgroup_charge_hugetlb(struct folio* folio, gfp_t gfp);
-
 int mem_cgroup_swapin_charge_folio(struct folio *folio, struct mm_struct *mm,
 				  gfp_t gfp, swp_entry_t entry);
 
@@ -1161,11 +1159,6 @@ static inline int mem_cgroup_charge(struct folio *folio,
 		struct mm_struct *mm, gfp_t gfp)
 {
 	return 0;
-}
-
-static inline int mem_cgroup_charge_hugetlb(struct folio* folio, gfp_t gfp)
-{
-        return 0;
 }
 
 static inline int mem_cgroup_swapin_charge_folio(struct folio *folio,
