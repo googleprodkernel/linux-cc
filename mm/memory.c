7a31801c7ef1935d096a98d69ca55c393648ded3
Ackerley Tng <ackerleytng@google.com>
2025-09-22 07:38:45 +0000

    mm: Export unmap_mapping_folio() for KVM
    guest_memfd needs a way to unmap a folio from all userspace processes. This
    is required as part of a folio's truncation process. The function
    unmap_mapping_folio() provides exactly this functionality.
    
    Move its declaration from the internal mm/internal.h to the public
    include/linux/mm.h and export the symbol.
    
    No functional change intended.
    
    Change-Id: Ibb3409142dd19c1614d495a6ebc333dbf62be198
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>

--- mm/memory.c
+++ mm/memory.c
@@ -4074,6 +4074,7 @@ void unmap_mapping_folio(struct folio *folio)
 					 last_index, &details);
 	i_mmap_unlock_read(mapping);
 }
+EXPORT_SYMBOL_FOR_MODULES(unmap_mapping_folio, "kvm");
 
 /**
  * unmap_mapping_pages() - Unmap pages from processes.
