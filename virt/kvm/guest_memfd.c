67b558e2c7051c49d21676352a370a557ccca0dd
Sean Christopherson <seanjc@google.com>
2025-09-29 14:28:48 -0700

    KVM: guest_memfd: Wire up kvm_get_memory_attributes() to per-gmem attributes
    Implement kvm_gmem_get_memory_attributes() for guest_memfd to allow the KVM
    core and architecture code to query per-GFN memory attributes.
    
    kvm_gmem_get_memory_attributes() finds the memory slot for a given GFN and
    queries the guest_memfd file's to determine if the page is marked as
    private.
    
    If vm_memory_attributes is not enabled, there is no shared/private tracking
    at the VM level. Install the guest_memfd implementation as long as
    guest_memfd is enabled to give guest_memfd a chance to respond on
    attributes.
    
    guest_memfd should look up attributes regardless of whether this memslot is
    gmem-only since attributes are now tracked by gmem regardless of whether
    mmap() is enabled.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Co-developed-by: Ackerley Tng <ackerleytng@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>

--- virt/kvm/guest_memfd.c
+++ virt/kvm/guest_memfd.c
@@ -509,6 +509,37 @@ static int kvm_gmem_mmap(struct file *file, struct vm_area_struct *vma)
 	return 0;
 }
 
+unsigned long kvm_gmem_get_memory_attributes(struct kvm *kvm, gfn_t gfn)
+{
+	struct kvm_memory_slot *slot = gfn_to_memslot(kvm, gfn);
+	struct inode *inode;
+
+	/*
+	 * If this gfn has no associated memslot, there's no chance of the gfn
+	 * being backed by private memory, since guest_memfd must be used for
+	 * private memory, and guest_memfd must be associated with some memslot.
+	 */
+	if (!slot)
+		return 0;
+
+	CLASS(gmem_get_file, file)(slot);
+	if (!file)
+		return 0;
+
+	inode = file_inode(file);
+
+	/*
+	 * Rely on the maple tree's internal RCU lock to ensure a
+	 * stable result. This result can become stale as soon as the
+	 * lock is dropped, so the caller _must_ still protect
+	 * consumption of private vs. shared by checking
+	 * mmu_invalidate_retry_gfn() under mmu_lock to serialize
+	 * against ongoing attribute updates.
+	 */
+	return kvm_gmem_get_attributes(inode, kvm_gmem_get_index(slot, gfn));
+}
+EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_gmem_get_memory_attributes);
+
 static struct file_operations kvm_gmem_fops = {
 	.mmap		= kvm_gmem_mmap,
 	.open		= generic_file_open,
