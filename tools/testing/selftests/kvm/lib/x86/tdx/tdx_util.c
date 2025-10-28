a9e7c423a4744e98b7f521ed4f70259add7f14b8
Isaku Yamahata <isaku.yamahata@intel.com>
2025-10-28 21:20:39 +0000

    KVM: selftests: TDX: Use KVM_TDX_CAPABILITIES to validate TDs' attribute configuration
    Make sure that all the attributes enabled by the test are reported as
    supported by both the TDX module and KVM. KVM filters out the attributes
    not supported by itself.
    
    This also exercises the KVM_TDX_CAPABILITIES ioctl.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>
    Co-developed-by: Sagi Shahar <sagis@google.com>
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Reviewed-by: Binbin Wu <binbin.wu@linux.intel.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Signed-off-by: Lisa Wang <wyihan@google.com>

--- tools/testing/selftests/kvm/lib/x86/tdx/tdx_util.c
+++ tools/testing/selftests/kvm/lib/x86/tdx/tdx_util.c
@@ -110,6 +110,18 @@ static void tdx_filter_cpuid(struct kvm_vm *vm,
 	free(tdx_cap);
 }
 
+static void tdx_check_attributes(struct kvm_vm *vm, u64 attributes)
+{
+	struct kvm_tdx_capabilities *tdx_cap;
+
+	tdx_cap = tdx_read_capabilities(vm);
+
+	/* Make sure all the attributes are reported as supported */
+	TEST_ASSERT_EQ(attributes & tdx_cap->supported_attrs, attributes);
+
+	free(tdx_cap);
+}
+
 void tdx_init_vm(struct kvm_vm *vm, u64 attributes)
 {
 	struct kvm_tdx_init_vm *init_vm;
@@ -128,6 +140,8 @@ void tdx_init_vm(struct kvm_vm *vm, u64 attributes)
 
 	memcpy(&init_vm->cpuid, cpuid, kvm_cpuid2_size(cpuid->nent));
 	free(cpuid);
+
+	tdx_check_attributes(vm, attributes);
 
 	init_vm->attributes = attributes;
 
