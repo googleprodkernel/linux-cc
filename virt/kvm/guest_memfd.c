86b4213116a498cd1154d0aa49454c251978c157
Ackerley Tng <ackerleytng@google.com>
2026-01-27 15:16:42 -0800

    KVM: guest_memfd: Handle lru_add fbatch refcounts during conversion safety check
    When checking if a guest_memfd folio is safe for conversion, its refcount
    is examined. A folio may be present in a per-CPU lru_add fbatch, which
    temporarily increases its refcount. This can lead to a false positive,
    incorrectly indicating that the folio is in use and preventing the
    conversion, even if it is otherwise safe. The conversion process might not
    be on the same CPU that holds the folio in its fbatch, making a simple
    per-CPU check insufficient.
    
    To address this, drain all CPUs' lru_add fbatches if an unexpectedly high
    refcount is encountered during the safety check. This is performed at most
    once per conversion request. Draining only if the folio in question may be
    lru cached.
    
    guest_memfd folios are unevictable, so they can only reside in the lru_add
    fbatch. If the folio's refcount is still unsafe after draining, then the
    conversion is truly deemed unsafe.
    
    Reviewed-by: Fuad Tabba <tabba@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>

--- virt/kvm/guest_memfd.c
+++ virt/kvm/guest_memfd.c
@@ -8,6 +8,7 @@
 #include <linux/mempolicy.h>
 #include <linux/pseudo_fs.h>
 #include <linux/pagemap.h>
+#include <linux/swap.h>
 
 #include "kvm_mm.h"
 
@@ -596,6 +597,7 @@ static bool kvm_gmem_is_safe_for_conversion(struct inode *inode, pgoff_t start,
 	const int filemap_get_folios_refcount = 1;
 	pgoff_t last = start + nr_pages - 1;
 	struct folio_batch fbatch;
+	bool lru_drained = false;
 	bool safe = true;
 	pgoff_t next;
 	int i;
@@ -605,12 +607,20 @@ static bool kvm_gmem_is_safe_for_conversion(struct inode *inode, pgoff_t start,
 	next = start;
 	while (safe && filemap_get_folios(mapping, &next, last, &fbatch)) {
 
-		for (i = 0; i < folio_batch_count(&fbatch); ++i) {
+		for (i = 0; i < folio_batch_count(&fbatch);) {
 			struct folio *folio = fbatch.folios[i];
 
-			if (folio_ref_count(folio) !=
-			    folio_nr_pages(folio) + filemap_get_folios_refcount) {
-				safe = false;
+			safe = (folio_ref_count(folio) ==
+				folio_nr_pages(folio) +
+				filemap_get_folios_refcount);
+
+			if (safe) {
+				++i;
+			} else if (folio_may_be_lru_cached(folio) &&
+				   !lru_drained) {
+				lru_add_drain_all();
+				lru_drained = true;
+			} else {
 				*err_index = max(start, folio->index);
 				break;
 			}
