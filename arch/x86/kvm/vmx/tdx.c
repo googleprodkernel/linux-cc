cf954c080affe1acf3424f9673bf9437ab74f96e
Ackerley Tng <ackerleytng@google.com>
2026-04-27 13:51:10 -0700

    KVM: TDX: Make source page optional for KVM_TDX_INIT_MEM_REGION
    Update tdx_gmem_post_populate() to handle cases where a source page is
    not explicitly provided. Instead of returning -EOPNOTSUPP when src_page
    is NULL, default to using the page associated with the destination PFN.
    
    This change allows for in-place memory conversion where the data is
    already present in the target PFN, ensuring the TDX module has a valid
    source page reference for the TDH.MEM.PAGE.ADD operation.
    
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -3116,8 +3116,12 @@ static int tdx_gmem_post_populate(struct kvm *kvm, gfn_t gfn, kvm_pfn_t pfn,
 	if (KVM_BUG_ON(kvm_tdx->page_add_src, kvm))
 		return -EIO;
 
-	if (!src_page)
-		return -EOPNOTSUPP;
+	if (!src_page) {
+		if (vm_memory_attributes)
+			return -EOPNOTSUPP;
+
+		src_page = pfn_to_page(pfn);
+	}
 
 	kvm_tdx->page_add_src = src_page;
 	ret = kvm_tdp_mmu_map_private_pfn(arg->vcpu, gfn, pfn);
@@ -3196,7 +3200,8 @@ static int tdx_vcpu_init_mem_region(struct kvm_vcpu *vcpu, struct kvm_tdx_cmd *c
 			break;
 		}
 
-		region.source_addr += PAGE_SIZE;
+		if (region.source_addr)
+			region.source_addr += PAGE_SIZE;
 		region.gpa += PAGE_SIZE;
 		region.nr_pages--;
 
