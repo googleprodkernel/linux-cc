aca6a88933aff8b084531a674ebc84848f1f2b5f
Sagi Shahar <sagis@google.com>
2026-07-22 23:13:10 +0000

    KVM: selftests: Expose segment definitions to assembly files
    Move kernel segment definitions to a separate file which can be included
    from assembly files.
    
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Reviewed-by: Binbin Wu <binbin.wu@linux.intel.com>
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Signed-off-by: Lisa Wang <wyihan@google.com>
    Reviewed-by: Xiaoyao Li <xiaoyao.li@intel.com>

---
+++ tools/testing/selftests/kvm/include/x86/processor_asm.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Used for storing defines used by both c and assembly code.
+ */
+#ifndef SELFTEST_KVM_PROCESSOR_ASM_H
+#define SELFTEST_KVM_PROCESSOR_ASM_H
+
+#define KERNEL_CS	0x8
+#define KERNEL_DS	0x10
+#define KERNEL_TSS	0x18
+
+#endif  /* SELFTEST_KVM_PROCESSOR_ASM_H */
