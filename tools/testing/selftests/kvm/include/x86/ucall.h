37e4ec88f57febc3d7174cb7dbd44ef9994638ea
Ackerley Tng <ackerleytng@google.com>
2026-07-22 23:13:26 +0000

    KVM: selftests: Add ucall support for TDX
    Implement TDX ucall using TDCALL-based MMIO to pass the ucall address
    from the VM to the host.
    
    In standard KVM selftests, ucall uses a PIO instruction as a trigger
    to exit to the host, which then retrieves the ucall address by reading
    the guest's RDI register. This approach is incompatible with TDX
    because the host cannot access guest registers.
    
    Furthermore, PIO exits only expose 4 bytes of immediate data, which
    is insufficient for a 8-byte ucall address. By using TDCALL-based MMIO,
    the VM can share the full 8-byte address in a single exit without
    refactoring the common ucall framework and other non-x86 architectures.
    
    Co-developed-by: Sagi Shahar <sagis@google.com>
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Co-developed-by: Lisa Wang <wyihan@google.com>
    Signed-off-by: Lisa Wang <wyihan@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>

--- tools/testing/selftests/kvm/include/x86/ucall.h
+++ tools/testing/selftests/kvm/include/x86/ucall.h
@@ -2,12 +2,6 @@
 #ifndef SELFTEST_KVM_UCALL_H
 #define SELFTEST_KVM_UCALL_H
 
-#include "kvm_util.h"
-
 #define UCALL_EXIT_REASON       KVM_EXIT_IO
-
-static inline void ucall_arch_init(struct kvm_vm *vm, gpa_t mmio_gpa)
-{
-}
 
 #endif
