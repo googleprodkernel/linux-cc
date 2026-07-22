558cb2fa4c9d25c4cc0784550e7d51cd0f8ad828
Ackerley Tng <ackerleytng@google.com>
2026-07-22 23:13:26 +0000

    KVM: selftests: Add ucall support for TDX
    Implement TDX ucall using TDCALL-based MMIO to pass the ucall address
    from the VM to the host.
    
    In standard KVM selftests, ucall uses a PIO instruction as a trigger
    to exit to the host, which then retrieves the ucall address by reading
    the guest's RDI register. This approach is incompatible with TDX
    because the host cannot access guest registers.
    
    Furthermore, PIO exits only expose 4 bytes of immediate data, which
    is insufficient for a 8-byte ucall address. By using TDCALL-based MMIO,
    the VM can share the full 8-byte address in a single exit without
    refactoring the common ucall framework and other non-x86 architectures.
    
    Co-developed-by: Sagi Shahar <sagis@google.com>
    Signed-off-by: Sagi Shahar <sagis@google.com>
    Co-developed-by: Lisa Wang <wyihan@google.com>
    Signed-off-by: Lisa Wang <wyihan@google.com>
    Signed-off-by: Ackerley Tng <ackerleytng@google.com>

--- tools/testing/selftests/kvm/lib/x86/ucall.c
+++ tools/testing/selftests/kvm/lib/x86/ucall.c
@@ -5,11 +5,35 @@
  * Copyright (C) 2018, Red Hat, Inc.
  */
 #include "kvm_util.h"
+#include "tdx/tdx.h"
+#include "tdx/tdx_util.h"
 
 #define UCALL_PIO_PORT ((u16)0x1000)
 
+/* HPET address is guaranteed to be unused for ucall MMIO */
+#define UCALL_MMIO_GPA 0xfed00000
+
+static u8 vm_type;
+static gpa_t ucall_mmio_gpa;
+
+void ucall_arch_init(struct kvm_vm *vm, gpa_t mmio_gpa)
+{
+	vm_type = vm->type;
+	sync_global_to_guest(vm, vm_type);
+
+	if (is_tdx_vm(vm)) {
+		ucall_mmio_gpa = UCALL_MMIO_GPA | vm->arch.s_bit;
+		sync_global_to_guest(vm, ucall_mmio_gpa);
+	}
+}
+
 void ucall_arch_do_ucall(gva_t uc)
 {
+	if (vm_type == KVM_X86_TDX_VM) {
+		tdx_mmio_write(ucall_mmio_gpa, sizeof(gva_t), uc);
+		return;
+	}
+
 	asm volatile("in %[port], %%al"
 		: : [port] "d" (UCALL_PIO_PORT), "D" (uc) : "rax", "memory");
 }
@@ -17,6 +41,15 @@ void ucall_arch_do_ucall(gva_t uc)
 void *ucall_arch_get_ucall(struct kvm_vcpu *vcpu)
 {
 	struct kvm_run *run = vcpu->run;
+
+	if (vm_type == KVM_X86_TDX_VM) {
+		if (run->exit_reason == KVM_EXIT_MMIO &&
+		    run->mmio.phys_addr == UCALL_MMIO_GPA &&
+		    run->mmio.len == sizeof(gva_t) &&
+		    run->mmio.is_write)
+			return (void *)(*((gva_t *)run->mmio.data));
+		return NULL;
+	}
 
 	if (run->exit_reason == KVM_EXIT_IO && run->io.port == UCALL_PIO_PORT) {
 		struct kvm_regs regs;
